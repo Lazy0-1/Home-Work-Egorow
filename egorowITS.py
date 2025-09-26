2917a7ac7637a8bee3c71b80812d35a49aac99b6
Snoxxx <andrey.dalech@gmail.com>
2025-09-26 09:24:27 +0400

    Update egorowITS.py

--- egorowITS.py
+++ egorowITS.py
@@ -1,4 +1,8 @@
-for i in range(1, 9):
-    print(i)
-for i in range(8, 0, -1):
-    print(i)
+def sosal?
+    a = sosal
+    for i in range(1, 9):
+        print(i)
+    for i in range(8, 0, -1):
+        print(i)
+    return a
+print(a)
